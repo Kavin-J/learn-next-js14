0cd6b314dd7bf87e88a945bd787bea2903d03e3c
Kavin24 <j.kavin2024@gmail.com>
2024-01-24 16:02:12 +0700

    Chapter 5 create dashboard invoices customer route

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+
+const Page = () => {
+  return (
+    <div>Customers Page</div>
+  )
+}
+ export default Page
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,10 @@
 import { Inconsolata } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
-export const inconsolata = Inconsolata({ subsets: ['latin'] });
+export const inconsolata = Inconsolata({
+  subsets: ['latin'],
+  weight: '500',
+  
+});
 export const lusitana = Lusitana({
   subsets: ['latin'],
   weight: ['400', '700'],
