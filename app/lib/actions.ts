ced55763025c8983b533ea6e6b0e845801b58ffd
Kavin24 <j.kavin2024@gmail.com>
2024-01-27 10:42:36 +0700

    update invoices , server action =>

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,7 +2,7 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -12,6 +12,7 @@ const FormSchema = z.object({
 });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+//Mock   generate Date Func
 function generateRandomDate(from: Date, to: Date): Date {
   return new Date(
     from.getTime() + Math.random() * (to.getTime() - from.getTime()),
@@ -28,11 +29,11 @@ export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
   const amountInCents = amount * 100;
   /** for Mock if you need to use real data comment this  and uncommment below statement*/
-    // const date = new Date(
-    //   generateRandomDate(new Date('2023-01-26'), new Date('2024-01-26')),
-    // )
-    //   .toISOString()
-    //   .split('T')[0];
+  // const date = new Date(
+  //   generateRandomDate(new Date('2023-01-26'), new Date('2024-01-26')),
+  // )
+  //   .toISOString()
+  //   .split('T')[0];
   /* for real data */
   const date = new Date().toISOString().split('T')[0];
   console.log({ customerId, amountInCents, status, date });
@@ -42,5 +43,25 @@ export async function createInvoice(formData: FormData) {
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
   revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function editInvoice(id: string, formData: FormData) {
+  console.log(id);
+  console.log(formData);
+
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: Number(formData.get('amount')),
+    status: formData.get('status'),
+  };
+  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+  const amountInCents = amount * 100;
+  await sql`
+    UPDATE INVOICES
+    SET CUSTOMER_ID = ${customerId} , AMOUNT = ${amountInCents} , STATUS = ${status} 
+    WHERE ID = ${id}
+  `
+  revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
